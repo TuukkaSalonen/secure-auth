dbdeaf322cb18ca0a6aed25d7ee46341fd2703b8
Tuukka Salonen <tuukka.t.salonen@tuni.fi>
2025-04-04 19:08:15 +0300

    Add more security headers

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     {
-      name: 'custom-middleware',
+      name: "custom-middleware",
       configureServer(server) {
         server.middlewares.use((_, res, next) => {
           res.setHeader("X-Frame-Options", "DENY");
           res.setHeader("X-Content-Type-Options", "nosniff");
-          res.setHeader("Content-Security-Policy", "default-src 'self';");
+          res.setHeader(
+            "Content-Security-Policy",
+            "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';"
+          );
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Permissions-Policy", "interest-cohort=()");
+          
           next();
         });
       },
