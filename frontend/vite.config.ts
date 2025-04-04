7d2f2eb510880186f1d2ddf3670a9870acb65c72
Tuukka Salonen <tuukka.t.salonen@tuni.fi>
2025-04-04 18:16:31 +0300

    Add frontend request headers

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'custom-middleware',
+      configureServer(server) {
+        server.middlewares.use((_, res, next) => {
+          res.setHeader("X-Frame-Options", "DENY");
+          res.setHeader("X-Content-Type-Options", "nosniff");
+          res.setHeader("Content-Security-Policy", "default-src 'self';");
+          next();
+        });
+      },
+    },
+  ],
+});
