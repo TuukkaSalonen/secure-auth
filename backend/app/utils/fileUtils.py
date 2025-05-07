e6fbdc5ebb20efaacfd90f0103641b7bf4b98377
Tuukka Salonen <tuukka.t.salonen@tuni.fi>
2025-05-07 22:47:16 +0300

    Modify comments

--- backend/app/utils/fileUtils.py
+++ backend/app/utils/fileUtils.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
 from cryptography.hazmat.primitives import padding
 
-# Encrypts file data using AES encryption with user specific key
+# Encrypts file data using AES-256 encryption with user specific key
 def encrypt_file(file_bytes, user):
     user_key = user.decrypt_user_key()
     
@@ -11,7 +11,7 @@ def encrypt_file(file_bytes, user):
     iv_file = os.urandom(16)
     iv_key = os.urandom(16)
 
-    # Pad data
+    # Pad data to 128-bit block size
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(file_bytes) + padder.finalize()
 
@@ -27,7 +27,7 @@ def encrypt_file(file_bytes, user):
 
     return encrypted_data, encrypted_aes_key, iv_file, iv_key
 
-# Decrypts file data using AES decryption with user specific key
+# Decrypts file data using AES-256 decryption with user specific key
 def decrypt_file(encrypted_data, encrypted_key, iv_file, iv_key, user):
     user_key = user.decrypt_user_key()
 
